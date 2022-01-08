681af28366be675ac278dd66cce318287ac9c90c
sponge <sponge@d8d.org>
2022-01-08 17:28:19 -0600

    if editing the interaction fails, send a new message and start updating that instead

--- index.mjs
+++ index.mjs
@@ -79,6 +79,21 @@ function createRichEmbed(game) {
   return embed;
 }
 
+// interaction tokens only last 15 minutes, but some folks may want to play a very slow version of the game
+// so send a new message if the interaction update fails
+async function tryUpdateInteraction(client, game) {
+  try {
+    await game.originalInteraction.editReply({ embeds: [createRichEmbed(game)] });
+  } catch (err) {
+    if (!game.originalMessage) {
+      await game.originalInteraction.guild.channels.fetch();
+      game.originalMessage = await client.channels.cache.get(game.originalInteraction.channelId).send({ embeds: [createRichEmbed(game)] });
+    } else {
+      await game.originalMessage.edit({ embeds: [createRichEmbed(game)] });
+    }
+  }
+}
+
 async function main() {
   // const rest = new REST({ version: '9' }).setToken(Config.token);
   // try {
@@ -118,7 +133,8 @@ async function main() {
           const game = startGame(wordLength);
           game.originalInteraction = interaction;
           games.set(interaction.channelId, game);
-          console.log(game);
+          console.log(`Started game: guild:${interaction.guildId} channel:${interaction.channelId} word:${game.word}`);
+          if (Config.verbose) console.log(game);
 
           await interaction.reply({ embeds: [createRichEmbed(game)] });
           return;
@@ -134,7 +150,7 @@ async function main() {
 
           const game = games.get(interaction.channelId);
           const result = guess(game, guessedWord);
-          console.log(game, result);
+          if (Config.verbose) console.log(game, result);
 
           if (!result.success) {
             await interaction.reply({ content: getErrorString(result.status), ephemeral: true });
@@ -145,15 +161,17 @@ async function main() {
 
           if (result.status == 'game-won') {
             reply += '\n**Game Over:** You win!';
-          }
-
-          if (result.status == 'game-lost') {
+            console.log(`Game won: guild:${interaction.guildId} channel:${interaction.channelId} word:${game.word}`);
+          } else if (result.status == 'game-lost') {
             reply += `\n**Game Over:** you lost!\nThe word was: **${game.word}**`;
+            console.log(`Game lost: guild:${interaction.guildId} channel:${interaction.channelId} word:${game.word}`);
+          } else if (result.status == 'guess-made') {
+            console.log(`Guess made: guild:${interaction.guildId} channel:${interaction.channelId} word:${game.word} guess:${result.guess.word}`);
           }
 
           await Promise.all([
             interaction.reply(reply),
-            game.originalInteraction.editReply({ embeds: [createRichEmbed(game)] }),
+            tryUpdateInteraction(client, game),
           ]);
 
           if (result.status === 'game-won' || result.status === 'game-lost') {
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 Play Wordle with your friends on your Discord server
 
 ## Invite
-[Add Discwordle to your server!](https://discord.com/api/oauth2/authorize?client_id=929196686243397672&permissions=2048&scope=bot%20applications.commands)
+[Add Discwordle to your server!](https://discord.com/api/oauth2/authorize?client_id=929196686243397672&permissions=264192&scope=bot%20applications.commands)
 
 ## About
 Discwordle is a simple bot that plays as many games of [Wordle](https://www.powerlanguage.co.uk/wordle/) as your heart desires. Currently, all players cooperate on the same board, with everyone getting the maximum six guesses.
