50700b6a9c43405449455070a81729a37d909e34
sponge <sponge@d8d.org>
2022-01-08 18:03:49 -0600

    comment up, add a bunch of TODOs

--- game.mjs
+++ game.mjs
@@ -2,72 +2,96 @@ import commonWords from './common_words.mjs';
 import dictionaryWords from './dictionary_words.mjs';
 import excludedWords from './excluded_words.mjs';
 
+// commonWords is sorted by usage, so grab the most used words
+// excludedWords is manually maintained, also came from hello wordl and just has some names
+// and proper nouns
 const validWords = commonWords
   .slice(0, 20000)
   .filter((word) => dictionaryWords.has(word) && !excludedWords.has(word));
 
-const errors = {
-  'wrong-word-length': 'The word you guessed is not the correct length.',
-  'not-a-word': 'The word you guessed is not a word.',
-}
-
+// returns a string or unknown error if for some reason i didn't update the errors dict above
 function getErrorString(status) {
+  const errors = {
+    'wrong-word-length': 'The word you guessed is not the correct length.',
+    'not-a-word': 'The word you guessed is not a word.',
+  }
+  
   return errors[status] ?? `An unknown error occurred. (${status})`;
 }
 
+// public function, starts a game. this function does not fail
+// (well not literally, i mean all code can go rogue, but rather there's not intended to be a 
+// failure state, no early returns or exceptions)
 function startGame(wordLength) {
+  // if the requested word length doesn't exist, default back to 5
+  // TODO: maybe just decrease by 1 every time, but it'd suck if you typed in 9999999999
   if (!validWords.some((word) => word.length == wordLength)) {
     wordLength = 5;
   }
 
+  // filter the word list down to words of that length and choose a random one
   const eligible = validWords.filter(word => word.length === wordLength);
   const word = eligible[Math.floor(Math.random() * eligible.length)];
   
+  // create the selected letters display
   const letters = {};
   'abcdefghijklmnopqrstuvwxyz'.split('').forEach(letter => letters[letter] = { letter, status: 'unknown' });
 
   return {
-    word,
-    splitWord: word.split(''),
-    guesses: [],
-    maxGuesses: 6,
-    letters,
-    status: 'game-started',
+    word, // the secret word in the game
+    splitWord: word.split(''), // just for convenience
+    guesses: [], // list of all guesses made and their correctness
+    maxGuesses: 6, // max guesses allowed, not configurable atm
+    letters, // status of which letters have been used, and if they were correct 
+    status: 'game-started', // current stsatus of the game
   }
 }
 
+// public function, guesses a letter in the game. if the input is invalid, it will return an
+// object with success: false, and a status that can be grabbed from getErrorString
+// TODO: maybe just include the error message in the object directly.
 function guess(game, guessedWord) {
+  // if the guess isn't the right length, don't bother
   if (guessedWord.length != game.word.length) {
     return { success: false, status: 'wrong-word-length' };
   }
 
+  // all guesses have to be valid words. no mashing keys to figure it out.
   if (!dictionaryWords.has(guessedWord)) {
     return { success: false, status: 'not-a-word' };
   }
 
+  // the guess is valid. build the object and figure it out
   const guess = {
     word: guessedWord,
     letters: []
   };
 
-  // FIXME: this sucks, probably dont need old for loops anymore
+  // make a copy of the letters in the word so we can remove them as they're used
+  // this is what makes duplicate letters only light up the amount of times they're used
   const remainingLetters = [...game.splitWord];
-  // first pass: correct letters only
-  // (ex. word is "glove" but you guess "geode", the first e needs to be gray)
+
+  // first pass: find correct letters only. this is separated out because ex. word is "glove" but you
+  // guess "geode", the first e needs to be gray, not yellow)
+  // TODO: this sucks, probably dont need old for loops anymore
   for (let i = 0; i < game.word.length; i++) {
     const correct = guessedWord[i] == game.splitWord[i];
-    const letterStatus = {
+
+    guess.letters.push({
       letter: guessedWord[i],
       status: correct ? 'correct' : 'unused'
-    }
+    });
 
     if (correct) remainingLetters.splice(remainingLetters.indexOf(guessedWord[i]), 1);
-    guess.letters.push(letterStatus);
   }
 
-  // second pass: correct letters in the wrong position
+  // second pass: find correct letters in the wrong position
+  // TODO: confusing, looping through guess.letters here since it now exists. create the object first
+  // and use the same loop twice?
   for (let i = 0; i < guess.letters.length; i++) {
+    // ignore correct letters
     if (guess.letters[i].status === 'correct') continue;
+
     const status = remainingLetters.includes(guessedWord[i]) ? 'in-word' : 'unused';
     guess.letters[i].status = status;
     if (status === 'in-word') {
@@ -76,7 +100,7 @@ function guess(game, guessedWord) {
   }
 
   // update the global letters status now, not allowing any status to get downgraded
-  // (this can probably be improved with enums and Math.max())
+  // TODO: his can probably be improved with enums and Math.max(). very confusing variable naming too!
   for (let i = 0; i < guess.letters.length; i++) {
     const guessLetter = guess.letters[i];
     const gameLetter = game.letters[guessLetter.letter];
@@ -91,7 +115,7 @@ function guess(game, guessedWord) {
 
   game.guesses.push(guess);
 
-  // update the game object with the status, too, and return the guess
+  // check game ending conditions, or just continue on
   if (game.word == guessedWord) {
     game.status = 'game-won';
     return { success: true, status: 'game-won', guess }
--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ import { Routes } from 'discord-api-types/v9';
 import { green, yellow, gray, darkGray } from './letters.mjs';
 import { startGame, guess, getErrorString } from './game.mjs';
 
+// our commands
 const commands = [
   {
     name: 'discwordle',
@@ -28,8 +29,11 @@ const commands = [
   }
 ];
 
+// mapping of channel id to the active game, this is the master list of games
 const games = new Map();
 
+// prints a string of letters with the correct color (this does not handle the letter display
+// because that has an extra state of "unused" vs "unknown")
 function printGuessLetters(letters) {
   let str = '';
 
@@ -41,13 +45,16 @@ function printGuessLetters(letters) {
   return str;
 }
 
+// creates the whole rich embed for the game, can be used in interactions or messages
 function createRichEmbed(game) {
+  // print the guesses so far
   let board = '';
 
   for (const guess of game.guesses) {
     board += `${printGuessLetters(guess.letters)}\n`;
   }
 
+  // print empty spaces for the rest
   for (let i = game.guesses.length; i < game.maxGuesses; i++) {
     for (let j = 0; j < game.word.length; j++) {
       board += '<:blank:929248507016118322> ';
@@ -55,20 +62,26 @@ function createRichEmbed(game) {
     board += '\n';
   }
 
+  // the letter display underneath the game board, note that we need to distinguish between
+  // "unknown" and "unused" here, otherwise we could use printGuessLetters
   let lettersStatus = '';
   for (const letter of Object.values(game.letters)) {
     const color = letter.status === 'correct' ? green :
       letter.status === 'in-word' ? yellow :
-        letter.status === 'unknown' ? gray :
-          darkGray;
+      letter.status === 'unknown' ? gray :
+      darkGray;
     lettersStatus += `${color[letter.letter]} `;
+    
+    // break halfway through
     if (letter.letter === 'm') lettersStatus += '\n';
   }
 
+  // build the actual embed
   const embed = new MessageEmbed()
     .setDescription(board)
     .addFields({ name: 'Letters', value: lettersStatus });
 
+  // decorate the board after the game ends
   if (game.status === 'game-won') {
     embed.setTitle('You win!');
   } else if (game.status === 'game-lost') {
@@ -83,8 +96,10 @@ function createRichEmbed(game) {
 // so send a new message if the interaction update fails
 async function tryUpdateInteraction(client, game) {
   try {
+    // TODO: once this happens once, it always fails. can maybe skip this by throwing an error to jump right to the catch
     await game.originalInteraction.editReply({ embeds: [createRichEmbed(game)] });
   } catch (err) {
+    // create a new message, or reuse an existing one
     if (!game.originalMessage) {
       await game.originalInteraction.guild.channels.fetch();
       game.originalMessage = await client.channels.cache.get(game.originalInteraction.channelId).send({ embeds: [createRichEmbed(game)] });
@@ -116,13 +131,16 @@ async function main() {
   });
 
   client.on('interactionCreate', async interaction => {
+    // wrap the whole game in a try/catch and end it if anything goes wrong
     try {
       if (!interaction.isCommand()) return;
 
       switch (interaction.commandName) {
+        // command for starting a new game
         case 'discwordle': {
           const wordLength = interaction.options.getInteger('length') ?? 5;
 
+          // if a game already is ongoing, reprint the message and start updating that one instead
           if (games.has(interaction.channelId)) {
             const game = games.get(interaction.channelId);
             game.originalInteraction = interaction;
@@ -130,9 +148,11 @@ async function main() {
             return;
           }
 
+          // create a new game
           const game = startGame(wordLength);
           game.originalInteraction = interaction;
           games.set(interaction.channelId, game);
+
           console.log(`Started game: guild:${interaction.guildId} channel:${interaction.channelId} word:${game.word}`);
           if (Config.verbose) console.log(game);
 
@@ -140,23 +160,30 @@ async function main() {
           return;
         }
 
+        // command for guessing a word
         case 'guess': {
+          // TODO: this probably shouldn't ever be blank since enforced by the client, but it'll kill the game if it somehow is
           const guessedWord = interaction.options.getString('word', true);
 
+          // no game ongoing
           if (!games.has(interaction.channelId)) {
-            await interaction.reply({ content: 'no game started!', ephemeral: true });
+            await interaction.reply({ content: 'No game active! Try /discwordle to start a new one.', ephemeral: true });
             return;
           }
 
+          // run the guess through the actual game logic
           const game = games.get(interaction.channelId);
           const result = guess(game, guessedWord);
+
           if (Config.verbose) console.log(game, result);
 
+          // usually just an input error
           if (!result.success) {
             await interaction.reply({ content: getErrorString(result.status), ephemeral: true });
             return;
           }
 
+          // print the guess with the correct colors, and print win/loss if applicable
           let reply = `${game.guesses.length}/${game.maxGuesses}: ${printGuessLetters(result.guess.letters)}`;
 
           if (result.status == 'game-won') {
@@ -169,11 +196,13 @@ async function main() {
             console.log(`Guess made: guild:${interaction.guildId} channel:${interaction.channelId} word:${game.word} guess:${result.guess.word}`);
           }
 
+          // update both at once, no need to wait for one or the other to finish
           await Promise.all([
             interaction.reply(reply),
             tryUpdateInteraction(client, game),
           ]);
 
+          // if the game is over, remove it from the active games list
           if (result.status === 'game-won' || result.status === 'game-lost') {
             games.delete(interaction.channelId);
           }
@@ -182,9 +211,12 @@ async function main() {
         }
       }
     } catch (error) {
+      // end the game if anything goes wrong
       console.error(error);
       console.error(error.stack);
       games.delete(interaction.channelId);
+
+      // just send a message to the channel, that usually won't fail unless something really terrible is going on
       try {
         await interaction.guild.channels.fetch();
         client.channels.cache.get(interaction.channelId).send(`Caught an exception, game over: ${error.message}`);
